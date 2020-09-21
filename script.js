e0de90695cf50d52cebafb0cc7a20752cb788269
ericmuh <muhwezi1000@gmail.com>
2020-09-21 18:00:53 +0300

    update todo

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const inputEl = document.querySelector(".inputEl");
 const addButton = document.querySelector(".addToDoBtn");
 const todos = document.querySelector(".todos");
 
+// // TODO Updating our todos
+//TODO Accounting for errors
 
-// * CRUD -create, read , delete
+// // * CRUD -create, read , delete, update
 
-// createTodoStore
+//* createTodoStore
 function createTodoStore() {
   let todostore = JSON.parse(localStorage.getItem("todos"));
   if (todostore === null) {
@@ -22,7 +24,7 @@ createTodoStore();
 function deleTodo() {
   const delButtons = document.querySelectorAll(".delBtn");
   let todostore = createTodoStore();
-console.log(delButtons);
+  console.log(delButtons);
   delButtons.forEach(function (button, index) {
     button.onclick = function () {
       todostore.splice(index, 1);
@@ -37,13 +39,40 @@ console.log(delButtons);
 //* create Todo
 function createTodo() {
   let todostore = createTodoStore();
-  todostore.push(inputEl.value);
+  const todoinputValue = inputEl.value;
+  
+  if (todoinputValue.trim().length !== 0) {
+    todostore.push(todoinputValue);
 
-  localStorage.setItem("todos", JSON.stringify(todostore));
+    localStorage.setItem("todos", JSON.stringify(todostore));
 
-  displayTodo();
-  deleTodo();
+    displayTodo();
+    deleTodo();
+    updateTodo();
+  }else{
+    alert("Your todo is empty")
+    return
+  }
 }
+
+// *updateTodo
+function updateTodo() {
+  const updateButtons = document.querySelectorAll(".updateBtn");
+  let todostore = createTodoStore();
+
+  updateButtons.forEach(function (button, index) {
+    button.onclick = function () {
+      const todo = this.parentElement.children[0];
+      const updateTodo = prompt(`updateTODO :${todo.innerText}`);
+      todostore.splice(index, 1, updateTodo);
+      localStorage.setItem("todos", JSON.stringify(todostore));
+      console.log(todostore);
+      displayTodo();
+      location.reload();
+    };
+  });
+}
+
 //* displayTodo
 function displayTodo() {
   let todostore = createTodoStore();
@@ -53,11 +82,18 @@ function displayTodo() {
   todostore.forEach(function (todoitem, itemid) {
     const singleTodo = document.createElement("p");
     const delbtn = document.createElement("button");
+    const updatebtn = document.createElement("button");
+
     delbtn.innerHTML = "delete";
     delbtn.className = "btn btn-danger delBtn";
 
-    singleTodo.innerText = todoitem;
+    updatebtn.innerHTML = "update";
+    updatebtn.className = "btn btn-warning updateBtn";
+
+    singleTodo.innerHTML = `<span>${todoitem}</span>`;
     singleTodo.appendChild(delbtn);
+    singleTodo.appendChild(updatebtn);
+
     delbtn.className = `btn btn-danger delBtn ${itemid} `;
 
     todos.appendChild(singleTodo); //inject each todo within the div of class todos.
@@ -65,5 +101,6 @@ function displayTodo() {
 }
 displayTodo();
 deleTodo();
+updateTodo();
 // inputEl.onchange = createTodo
 addButton.onclick = createTodo;
